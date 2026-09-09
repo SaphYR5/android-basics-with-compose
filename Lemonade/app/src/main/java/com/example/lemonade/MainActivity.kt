27a8c3c96723927a8136d0827470571a6a408ca9
SaphYR <samuel.h.ngo@gmail.com>
2026-09-09 16:28:18 +0100

    feat: display image in centre page

--- Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
+++ Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -4,12 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.lemonade.ui.theme.LemonadeTheme
 
@@ -27,13 +34,24 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun LemonadeScreen(modifier: Modifier = Modifier) {
-
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(R.drawable.lemon_tree),
+            contentDescription = stringResource(R.string.lemon_tree)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun LemonadeApp() {
     LemonadeTheme {
-        LemonadeScreen(modifier = Modifier)
+        LemonadeScreen(modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentSize(Alignment.Center)
+        )
     }
 }
