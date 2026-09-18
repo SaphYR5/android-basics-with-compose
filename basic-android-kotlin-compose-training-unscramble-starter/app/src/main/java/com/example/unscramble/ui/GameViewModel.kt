794381cc5d54f0d9606c9f8fe144419d15726982
SaphYR <samuel.h.ngo@gmail.com>
2026-09-18 17:24:27 +0100

    feat: add state flow to game view model

---
+++ basic-android-kotlin-compose-training-unscramble-starter/app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.unscramble.ui
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class GameViewModel : ViewModel() {
+    private val _uiState = MutableStateFlow(GameUIState())
+    val uiState: StateFlow<GameUIState> = _uiState.asStateFlow()
+}
